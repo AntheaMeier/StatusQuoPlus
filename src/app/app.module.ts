e6acfdb99f8c1e4ca21bb2e347b62f8bd1ebf7c2
Ceyda Patan <cpatan@icloud.com>
2021-10-31 19:13:50 +0100

    material for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatToolbarModule } from '@angular/material/toolbar';
-
- import { MatButtonModule } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import { ResponsiveHeaderComponent } from './responsive-header/responsive-header.component';
@@ -19,9 +15,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatCardModule} from "@angular/material/card";
 import { LoginZweiComponent } from './login-zwei/login-zwei.component';
-
-
-
+import { MatInputModule } from '@angular/material/input';
 
 
 
@@ -47,7 +41,7 @@ import { LoginZweiComponent } from './login-zwei/login-zwei.component';
     FormsModule,
     ReactiveFormsModule,
     MatCardModule,
-    // MatListModule
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
