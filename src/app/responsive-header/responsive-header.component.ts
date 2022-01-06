a05145c5576b1a1aa91dbdc6ed3b74d6e044407b
Lara Kiyicioglu <s0574645@htw-berlin.de>
2022-01-06 17:30:24 +0100

    Mitarbeiter in der Sidenav hinzugefügt

--- src/app/responsive-header/responsive-header.component.ts
+++ src/app/responsive-header/responsive-header.component.ts
@@ -117,6 +117,7 @@ export class ResponsiveHeaderComponent {
   onSelectVorgesetzte_r() {
     this.selectedRole = "Vorgesetzte_r"
     this.tasksToOneGoal = [];
+    this.reviewsToOneUser = [];
     this.clickedOnMitarbeiter= false;
   }
 
