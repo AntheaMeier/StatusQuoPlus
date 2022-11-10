393a1206344f130652a0a332b28cd982c90862c4
KarimaElSharkawy <92715986+KarimaElSharkawy@users.noreply.github.com>
2022-11-10 13:34:36 +0100

    backend completed Goal

---
+++ src/app/components/goals/goal-completed-dialog/goal-completed-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goal-completed-dialog',
+  templateUrl: './goal-completed-dialog.component.html',
+  styleUrls: ['./goal-completed-dialog.component.css']
+})
+export class GoalCompletedDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
