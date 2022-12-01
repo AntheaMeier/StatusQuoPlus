39576d114a6f3e8f88ae06e2848b98126f04083b
Seher Dogangüzel <seker.seher@hotmail.de>
2022-12-01 11:07:48 +0100

    Feedback navigation header and sidebar

---
+++ src/app/components/feedback/feedback-create/feedback-create.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import { FeedbackDialogComponent } from '../feedback-dialog/feedback-dialog.component';
+
+@Component({
+  selector: 'app-feedback-create',
+  templateUrl: './feedback-create.component.html',
+  styleUrls: ['./feedback-create.component.css']
+})
+export class FeedbackCreateComponent implements OnInit {
+  constructor(public dialog: MatDialog) {}
+ 
+ 
+
+  ngOnInit(): void {
+  }
+  openDialog() {
+    this.dialog.open(FeedbackDialogComponent);
+  }
+
+
+  
+}
---
+++ src/app/components/feedback/feedback-dialog/feedback-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feedback-dialog',
+  templateUrl: './feedback-dialog.component.html',
+  styleUrls: ['./feedback-dialog.component.css']
+})
+export class FeedbackDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
