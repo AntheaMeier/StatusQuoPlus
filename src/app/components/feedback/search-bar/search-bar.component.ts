4ff03f68a280f3ffe99fd54c11b3858c896e28d4
laura.kutschan <laura.kutschan@adesso.de>
2022-12-05 11:42:23 +0100

    search bar feedback

---
+++ src/app/components/feedback/search-bar/search-bar.component.ts
@@ -0,0 +1,47 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {ApiService} from "../../../services/api.service";
+import {FormControl} from "@angular/forms";
+import {Observable} from "rxjs";
+import {map, startWith} from "rxjs/operators";
+import {User} from "../../../models/user";
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent {
+
+  users: User[] = [];
+  myControl = new FormControl('');
+  filteredUsers?: Observable<User[]>;
+  @Output() receiverId = new EventEmitter<string>();
+
+  constructor(private api: ApiService) {
+    this.getAllUsers();
+    this.filteredUsers = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(state => (state ? this.filterStates(state) : this.users.slice())),
+    );
+  }
+
+  getAllUsers() {
+    this.api.getUsers().subscribe( (res: any) => {
+      res.forEach( (user: { firstname: string; surname: string; _id: any; }) => {
+        let userTemp: User = {name: (user.firstname + ' ' + user.surname), _id: user._id};
+        console.log(userTemp);
+        this.users.push(userTemp);
+      })
+    });
+  }
+
+  filterStates(value: string): User[] {
+    const filterValue = value.toLowerCase();
+    return this.users.filter(user => user.name.toLowerCase().includes(filterValue));
+  }
+
+  chooseUser(userId: string): void {
+    console.log('hey : ' + userId);
+    this.receiverId.emit(userId);
+  }
+}
--- src/app/models/feedback.ts
+++ src/app/models/feedback.ts
@@ -1,6 +1,6 @@
 export interface Feedback {
   _id: string;
-  providerId: string;
-  receiverId: string;
-  feedbackText: string;
+  provider_id: string;
+  receiver_id: string;
+  feedback_text: string;
 }
