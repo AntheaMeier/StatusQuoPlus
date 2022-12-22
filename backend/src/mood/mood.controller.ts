877a8a35490ec41d7a9427df1925723e39496ddc
KarimaElSharkawy <92715986+KarimaElSharkawy@users.noreply.github.com>
2022-12-22 13:06:30 +0100

    US 17: Backend angepasst

--- backend/src/mood/mood.controller.ts
+++ backend/src/mood/mood.controller.ts
@@ -12,4 +12,22 @@ import {
 @Controller('mood')
 export class MoodController {
   constructor(private moodService: MoodService) {}
+  @Post()
+    async addMood(
+      @Body('creator_id') creatorId: string,
+      @Body('creator_name') creatorname: string,
+      @Body('creation_date') date: Date,
+      @Body('emotion') emotion: string,
+      @Body('text') text: string,
+    ) {
+      console.log(creatorId + " " + creatorname + " " + date + " " + emotion + " " + text);
+      const generatedId = await this.moodService.insertMood(
+        creatorId,
+        creatorname,
+        date,
+        emotion,
+        text,
+      );
+      return { id: generatedId };
+    }
 }
--- backend/src/mood/mood.service.ts
+++ backend/src/mood/mood.service.ts
@@ -6,4 +6,23 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 @Injectable()
 export class MoodService {
   constructor(@InjectModel('Mood') private readonly moodModel: Model<Mood>) {}
+
+   async insertMood(
+      creator_id: string,
+      creator_Name: string,
+      text:string,
+      creation_date: Date,
+      emotion: string,
+
+    ) {
+      const newMood = new this.moodModel({
+        creator_id: creator_id,
+        creator_Name: creator_Name,
+        text: text,
+        creation_date: creation_date,
+        emotion: emotion,
+        });
+      const result = await newMood.save();
+      return result.id;
+    }
 }
